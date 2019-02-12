11d3a3a0aac0db3496052a43250ef2ebf570c3ba
Francisco Cruz <cruzelante098@gmail.com>
2019-02-12 09:56:33 +0000

    Stub

---
+++ src/ejemplos/junit/ejemplo1.java
@@ -0,0 +1,18 @@
+package ejemplos.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ejemplo1 {
+
+    @Test
+    void test() {
+        assertEquals(235, 23);
+    }
+    
+    void otroTest() {
+        
+    }
+
+}
